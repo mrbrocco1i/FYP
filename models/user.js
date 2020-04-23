b726f6d63fb5799ed2df1e95708b161d544d37e5
Wuzhen Ye <yewuzhen5736@gmail.com>
2020-04-23 03:17:27 +0100

    Revert "final version 2"
    This reverts commit dd5451a906113548a9a9ffeacc51f913b1f4171c.

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
     username: String,
     password: String,
     email: String,
+    total_ranking: Number,
     /*commodity_ids: [{type: Schema.Types.ObjectId, ref: 'commodity'}],
     transaction_ids: [{type: Schema.Types.ObjectId, ref: 'transaction'}],
     complaint_ids: [{type: Schema.Types.ObjectId, ref: 'complaint'}],*/
